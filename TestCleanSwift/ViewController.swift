9a123252df93c487379fde4b0953b5fb3525ab59
Martin Bing <bing.martin.photography@gmail.com>
2018-10-31 21:35:38 +0100

    Initial Commit

---
+++ TestCleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCleanSwift
+//
+//  Created by Martin Demiddel on 31.10.18.
+//  Copyright © 2018 Martin Bing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
